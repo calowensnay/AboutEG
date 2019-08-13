1acaedb4a17f3e9f4b4017f64ff41f6edb4f3e5b
carmelo <cowono@arplatia.com>
2019-08-13 20:54:24 +0100

    Probando

---
+++ AboutEG/AboutEG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AboutEG.Startup))]
+namespace AboutEG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
